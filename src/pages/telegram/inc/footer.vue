f600ee0ed47f4efb4b590f53b5d94e4a6c9c9de4
Alexey <blockstar2k@gmail.com>
2025-07-19 16:50:27 +0300

    share contact

---
+++ src/pages/telegram/inc/footer.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="mt-10 mb-4">
+    <h3 class="text-h6 font-weight-bold mb-2">Есть вопросы?</h3>
+    <p class="text-body-2 grey--text text--darken-1 mb-2">
+      Напишите в поддержку по любому вопросу и мы поможем вам. Мы на связи!
+    </p>
+    <v-btn
+      variant="text"
+      href="https://t.me/wbd_spport"
+      target="_blank"
+      class="pa-0 text-capitalize font-weight-bold"
+      min-width="auto"
+    >
+      @wbd_support
+      <v-icon right>mdi-arrow-right</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/pages/telegram/inc/header.vue
@@ -0,0 +1,49 @@
+<script setup>
+import logo from '@images/logo.svg?url'
+
+const closePage = () => {
+  // Logic to close the page (e.g., go back in history, close a dialog)
+  console.log('Close button clicked');
+}
+</script>
+
+<template>
+  <v-app-bar flat class="px-4" height="64px">
+    <v-btn text class="pa-0" min-width="auto" @click="closePage">
+      <v-icon left>mdi-close</v-icon>
+      <span class="text-body-1 text-capitalize">Закрыть</span>
+    </v-btn>
+
+    <v-spacer></v-spacer>
+
+    <img
+      :src="logo"
+      alt="WBDiscount"
+      height="40"
+      width="40"
+      class="rounded-lg"
+    />
+    <v-spacer></v-spacer>
+
+    <v-menu offset-y left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon v-bind="attrs" v-on="on">
+          <v-icon>ri-arrow-down-s-line</v-icon>
+          <v-icon>ri-more-fill</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>Опция 1</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Опция 2</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<style scoped lang="scss">
+
+</style>
